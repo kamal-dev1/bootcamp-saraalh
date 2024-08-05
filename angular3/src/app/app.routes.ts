a92f4a14663172ae4fa055058d86bfda090d055b
kamal gharejeloo <kamal.gharejeloo@outlook.com>
2024-08-05 21:01:52 +0330

    form control

--- angular3/src/app/app.routes.ts
+++ angular3/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { NotFoundComponent } from './404/404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:"form",
+    component: FormComponent
+  },
+  {
+    path:"404",
+    component: NotFoundComponent
+  },
+  {
+    path:"**",
+    component: NotFoundComponent
+  },
+  // {
+  //   path:"",
+  //   redirectTo:"form",
+  //   pathMatch:"full"
+  // },
+
+];
---
+++ angular3/src/app/form/form.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+  constructor(private formBuilder: FormBuilder){
+        this.createForm()
+
+
+  }
+  list:any[]=["dskfhsdkjfh"]
+
+
+  form:any
+  error:string=""
+  submitForm(){
+    console.log(this.form?.value);
+    if (!this.form.valid) {
+      this.error="نام اشتباه است"
+
+
+    }
+
+
+  }
+
+
+  createForm(){
+    this.form=this.formBuilder.group({
+      name:['',[Validators.required,Validators.minLength(3)]],
+      mobile:['']
+    })
+
+
+    //  this.form=new FormGroup({
+    //     name: new FormControl('kamal'),
+    //     mobile: new FormControl(''),
+    //   })
+  }
+}
